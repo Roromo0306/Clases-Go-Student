2ce3e5398d02b77d79c762c56776988ac233e0f3
Roromo0306 <pownyrainbow03@gmail.com>
2025-09-07 17:54:04 +0200

    CreacionProyecto

---
+++ Clase_GoStudent_2D/Clase_GoStudent_2D/Assets/MoverBala.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverBala : MonoBehaviour
+{
+    public float velocidad = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up *  velocidad * Time.deltaTime, Space.Self);
+    }
+}
