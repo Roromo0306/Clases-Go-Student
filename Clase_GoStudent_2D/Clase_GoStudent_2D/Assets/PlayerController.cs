d1c2e4d8a1f011c14fa5c0ffa1ecad0735d18e8d
Roromo0306 <pownyrainbow03@gmail.com>
2025-09-10 14:20:14 +0200

    Barra de Vida

--- Clase_GoStudent_2D/Clase_GoStudent_2D/Assets/PlayerController.cs
+++ Clase_GoStudent_2D/Clase_GoStudent_2D/Assets/PlayerController.cs
@@ -7,14 +7,22 @@ public class PlayerController : MonoBehaviour
     [Header("Atributos Player")]
     [Tooltip("Velocidad a la que se mueve el jugador")]
     public float velocidad = 5f;
+    public float vida;
+    public float vidaMaxima;
+    public BarraDeVida barraVida;
+
 
     private float MoveX, MoveY;
     private Rigidbody2D rb;
+
+   
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        vida = vidaMaxima;
+        barraVida.InicializarBarraVida(vida);
     }
 
     // Update is called once per frame
@@ -25,5 +33,15 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movimiento = new Vector3(MoveX , MoveY, 0f).normalized;
         rb.velocity = movimiento * velocidad;
+    }
+
+    public void Danio(float daño)
+    {
+        vida -= daño;
+        barraVida.CambiarVidaActual(vida);
+        if(vida <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
